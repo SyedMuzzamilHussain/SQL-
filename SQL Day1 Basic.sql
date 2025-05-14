b76a17163fe257a1091acc54064c4fbc05430f07
SyedMuzzamilHussain <smuzzamil497@gmail.com>
2025-05-14 11:03:06 +0530

    Add files via upload

---
+++ SQL Day1 Basic.sql
@@ -0,0 +1,14 @@
+select database();
+use sakila;
+select database();
+show databases;
+show tables;
+##how to access some column from this table data
+select * from actor;
+
+select actor_id,last_name,first_name,first_name from actor;
+
+##describe show our information for out table
+describe actor;
+
+
