155a1291a437441be102fe57c217a2d9c36eea6a
SyedMuzzamilHussain <smuzzamil497@gmail.com>
2025-05-21 18:25:13 +0530

    Add files via upload

---
+++ Day 7 SelfJoin.sql
@@ -0,0 +1,13 @@
+-- self join
+use sakila;
+show tables;
+create table employee2(eid int,ename varchar(20),manager_id int);
+insert into employee2 values(10,'tushar',null),(11,'aman',12),(12,'adi',10),(13,'sakshi',11);
+select * from employee2;
+-- combinations of columns is called candidate key
+select emp.eid,emp.ename,emp.manager_id,manager.eid,manager.ename from employee2 as emp
+join employee2 as manager where emp.manager_id= manager.eid;
+
+-- Natural join : It is worst join,if commom element works as inner join if all diffrent colunmn then work as a cartisan join.
+-- table which is small then we place it in left small and vice versa.
+
