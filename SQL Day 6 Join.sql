83076345c07beb9c5e8e6d27cc705262821981fe
Syed Muzzamil Hussain <smuzzamil497@gmail.com>
2025-05-29 21:02:45 +0530

    Add files via upload

---
+++ SQL Day 6 Join.sql
@@ -0,0 +1,22 @@
+-- basic idea of JOINS :
+use sakila;
+
+-- types of relationships 
+-- 1 to 1 = 
+
+
+
+
+
+
+
+
+-- 1 to many 
+-- many to many 
+-- many to none ( self refrencing) 
+
+-- what are keys ?
+-- primary key 
+-- super key 
+-- candidate key 
+
